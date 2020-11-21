6b44a6e8e30b22d19d0ce21fd7c6512edb365f8f
patschi1983 <patschi1983@users.noreply.github.com>
2020-11-21 21:34:13 +0100

    Update Configuration.h

--- FYSTEC F6/Marlin/Configuration.h
+++ FYSTEC F6/Marlin/Configuration.h
@@ -946,7 +946,7 @@
  * For other boards you may need to define FIL_RUNOUT_PIN, FIL_RUNOUT2_PIN, etc.
  * By default the firmware assumes HIGH=FILAMENT PRESENT.
  */
-//#define FILAMENT_RUNOUT_SENSOR
+#define FILAMENT_RUNOUT_SENSOR
 #if ENABLED(FILAMENT_RUNOUT_SENSOR)
   #define NUM_RUNOUT_SENSORS   1     // Number of sensors, up to one per extruder. Define a FIL_RUNOUT#_PIN for each.
   #define FIL_RUNOUT_INVERTING false // set to true to invert the logic of the sensor.
@@ -1148,7 +1148,7 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-#define MANUAL_X_HOME_POS 18
+//#define MANUAL_X_HOME_POS 0
 #define MANUAL_Y_HOME_POS -27
 //#define MANUAL_Z_HOME_POS 0
 
